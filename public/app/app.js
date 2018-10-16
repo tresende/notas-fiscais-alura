9e1b4b29edd744ff3f41dec06ca1d2cebd80b7a3
Thiago Resende <thiago.gcresende@gmail.com>
2018-10-15 21:18:50 -0300

    Deixando clara nossa intenção

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,15 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 
+const sumItems = code => notas => notas.$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
 document
     .querySelector('#myButton')
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(notas => notas
-                .$flatMap(nota => nota.itens)
-                .filter(item => item.codigo == '2143')
-                .reduce((total, item) => total + item.valor, 0)
-            )
-            .then(console.log)
-            .catch(console.log);
+            .then(sumItems('2143'))
+            .then(log)
+            .catch(log);
