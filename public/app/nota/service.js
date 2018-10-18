f43e83be58c7723e0c2c6d6b3feb158afabf0be3
Thiago Resende <thiago.gcresende@gmail.com>
2018-10-17 22:19:53 -0300

    atualizacao

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,6 +1,11 @@
 import { handleStatus } from './../utils/promise-helpers.js';
 const API = 'http://localhost:3000/notas'
 
+
+const getItemsFromNota = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+
 const sumItems = code => notas => notas.$flatMap(nota => nota.itens)
     .filter(item => item.codigo == code)
     .reduce((total, item) => total + item.valor, 0);
