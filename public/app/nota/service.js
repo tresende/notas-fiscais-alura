c8ef40c50d0b84de1505811d46a918bb7c0fe4e5
Thiago Resende <thiago.gcresende@gmail.com>
2018-10-27 16:26:10 -0300

    Compondo funções

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,25 +1,35 @@
 import { handleStatus } from './../utils/promise-helpers.js';
-import { partialize } from '../utils/operators';
+import { partialize } from './../utils/operators.js';
 const API = 'http://localhost:3000/notas'
 
-
-
-const getItemsFromNota = notas => notas.$flatMap(nota => nota.itens);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
 const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
 const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
-
 
 const sumItems = code => notas => notas.$flatMap(nota => nota.itens)
     .filter(item => item.codigo == code)
     .reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
-    listaAll() {
-        return fetch(API).then(handleStatus);
+    listAll() {
+        return fetch(API)
+            .then(handleStatus)
+            .catch(err => {
+                console.log(err);
+                return Promise.reject('Não foi possível obter as notas fiscais');
+            });
     },
 
     sumItems(code) {
+        // utilizando partialize
         const filterItems = partialize(filterItemsByCode, code);
-        return this.listaAll().then(sumItems(code));
+        // realizando a composição
+        return this.listAll().then(notas =>
+            sumItemsValue(
+                filterItems(
+                    getItemsFromNotas(notas)
+                )
+            )
+        );
     }
 }
